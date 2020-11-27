9012b1e122f9be619a304bf3e3e795c7ceb0b736
lukai10 <l.golebiowski7@gmail.com>
2020-11-27 09:49:21 +0100

    .

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <h1>Laboratorium z programowania w chmurze obliczeniowej</h1>
-<h4>Próba połączenia z baza MySQL</h4>
+<h4>Próba połączenia z baza MySQL:</h4>
 <?php 
 $host = 'mysql';
 $user = 'root';
@@ -9,5 +9,11 @@ $conn = new mysqli($host, $user, $pass);
 if ($conn->connect_error) {
     die("Połączenie z bazą MySQL nie powiodło się " . $conn->connect_error);
 } 
-echo "Połączenie z bazą MySQL powiodło się!";
+echo "Połączenie z bazą MySQL powiodło się!\n";
+
+echo "Informacje o serwisie php:";
+
+phpinfo(); 
 ?>
+
+
